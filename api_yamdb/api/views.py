95e222ba41c0a3de01d56da9dc1fcac180a6ac24
Илиан Хороших <ilian10@mail.ru>
2023-04-27 20:58:31 +0300

    Сделал Регу(Пока без отправки на почту)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,25 +11,6 @@ from reviews.models import User
 import six
 
 
-# class APIGetToken(APIView):
-#     def post(self, request):
-#         serializer = GetTokenSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         data = serializer.validated_data
-#         try:
-#             user = User.objects.get(username=data['username'])
-#         except User.DoesNotExist:
-#             return Response(
-#                 {'username': 'Пользователь не найден!'},
-#                 status=status.HTTP_404_NOT_FOUND)
-#         if default_token_generator.check_token(user, confirmation_code)
-#             return Response({'token': str(token)},
-#                             status=status.HTTP_201_CREATED)
-#         return Response(
-#             {'confirmation_code': 'Неверный код подтверждения!'},
-#             status=status.HTTP_400_BAD_REQUEST)
-
-
 class APISignUp(APIView):
     permission_classes = (permissions.AllowAny,)
 
