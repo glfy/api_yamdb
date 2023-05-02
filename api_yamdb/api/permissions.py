7bec86f18cd95a311951754c2a7098f518ad5959
artgele <120719728+artgele@users.noreply.github.com>
2023-05-02 19:23:26 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,7 +1,6 @@
 from rest_framework import permissions
 
 
-<<<<<<< HEAD
 class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
@@ -21,11 +20,6 @@ class AdminModeratorAuthorOrReadOnly(permissions.BasePermission):
                 or request.user.is_authenticated)
 
 
-class Admin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.is_admin or request.user.is_superuser)
-=======
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
@@ -38,4 +32,3 @@ class AdminOnly(permissions.BasePermission):
             request.user.is_admin
             or request.user.is_staff
         )
->>>>>>> 642c0042ff7db187b170eb471443adf2c8ecde97
